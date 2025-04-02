76c9fbffb2185c36a8d3bda0882e5a4e1bfc6956
manindersingh271@gmail.com <manindersingh271@gmail.com>
2025-04-02 14:11:08 +0530

    First commit

---
+++ src/main/java/com/oati/maninder_spring_azure_demo/ManinderSpringAzureDemoApplication.java
@@ -0,0 +1,22 @@
+package com.oati.maninder_spring_azure_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ManinderSpringAzureDemoApplication {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Congrats!!! ManinderSpringAzureDemo is successfull.";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ManinderSpringAzureDemoApplication.class, args);
+		System.out.println("----Started ManinderSpringAzureDemoApplication---");
+	}
+
+}
---
+++ src/test/java/com/oati/maninder_spring_azure_demo/ManinderSpringAzureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oati.maninder_spring_azure_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManinderSpringAzureDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
